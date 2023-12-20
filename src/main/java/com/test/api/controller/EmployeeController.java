db9ceb7492d887c43bd75b1482ad35a1d2829739
Angelo Dalisay <adalisay@utah.gov>
2023-12-20 16:52:16 -0700

    the problem

--- src/main/java/com/test/api/controller/EmployeeController.java
+++ src/main/java/com/test/api/controller/EmployeeController.java
@@ -11,13 +11,11 @@ import java.util.List;
 public class EmployeeController {
     private final List<Employee> employees = new ArrayList<>();
 
-    // Endpoint to retrieve all employees
     @GetMapping
     public List<Employee> getAllEmployees() {
         return employees;
     }
 
-    // Endpoint to retrieve details of a specific employee
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable int id) {
         for (Employee employee : employees) {
@@ -26,10 +24,9 @@ public class EmployeeController {
             }
         }
 
-        return null; // Employee with the specified ID not found
+        return null;
     }
 
-    // Endpoint to add a new employee
     @PostMapping
     public Employee addEmployee(@RequestBody Employee employee) {
         employee.setId(employees.size() + 1);
@@ -37,43 +34,17 @@ public class EmployeeController {
         return employee;
     }
 
-    // Endpoint to update information of an existing employee
+
     @PutMapping("/{id}")
     public Employee updateEmployee(@PathVariable int id, @RequestBody Employee updatedEmployee) {
-        for (Employee employee : employees) {
-            if (employee.getId() == id) {
-                employee.setName(updatedEmployee.getName());
-                employee.setPosition(updatedEmployee.getPosition());
-                employee.setSalary(updatedEmployee.getSalary());
-                return employee;
-            }
-        }
 
-        return null; // Employee with the specified ID not found
     }
 
-    // Endpoint to remove specific information of an existing employee
     @PatchMapping("/{id}")
     public Employee patchEmployee(@PathVariable int id, @RequestBody Employee updatedEmployee) {
-        for (Employee employee : employees) {
-            if (employee.getId() == id) {
-                if (updatedEmployee.getName() != null) {
-                    employee.setName(updatedEmployee.getName());
-                }
-                if (updatedEmployee.getPosition() != null) {
-                    employee.setPosition(updatedEmployee.getPosition());
-                }
-                if (updatedEmployee.getSalary() != 0) {
-                    employee.setSalary(updatedEmployee.getSalary());
-                }
-                return employee;
-            }
-        }
 
-        return null; // Employee with the specified ID not found
     }
 
-    // Endpoint to remove an existing employee
     @DeleteMapping("/{id}")
     public String deleteEmployee(@PathVariable int id) {
         for (Employee employee : employees) {
